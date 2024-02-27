e9002a0abeeb953a7cdb01e8ac57b94e18c669ab
SebasDev <Sebasfallas92@gmail.com>
2024-02-26 22:04:33 -0600

    fix server error

--- client/ecommerce-camisetas-futbol/src/secondaryComponents/ButtonOne.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/ButtonOne.css"
-
-function ButtonOne({onClick,content}){
-    return (
-        <button className="btn-1" onClick={onClick}>{content}</button>
-    )
-}
-
-export default ButtonOne;
--- client/ecommerce-camisetas-futbol/src/secondaryComponents/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/Logo.css";
-
-function Logo({className}){
-    return (
-        <h2 className={className || "header-h2"}>FutTeeStore</h2>
-    )
-}
-
-export default Logo;
